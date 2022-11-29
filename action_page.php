26690d44be79596143726cb11bcb67a4b3939a78
JeplnRunsYou <108194777+Jepln@users.noreply.github.com>
2022-11-29 21:33:55 +0000

    Initial commit

---
+++ action_page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>action_page.php</title>
+
+    
+  </head>
+
+  <body>
+    <h1>404 Not Found</h1>
+    <p>If problem persists, please contact site owner for assistance.</p>
+
+    <a href="/"><button>Go back</button></a>
+  </body>
+  
+</html>
